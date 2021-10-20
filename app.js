d43a81379c7efe421d68f451ac620e1ef5f41e1b
Shmee <smaheilman@gmail.com>
2021-10-19 20:38:14 -0400

    add html

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = (userName, githubName) => {
+    return`
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Portfolio Demo</title>
+    </head>
 
-    console.log('=========');
-
-    profileDataArr.forEach(profileItem =>
-        console.log(profileItem));
-    
+    <body>
+     <h1>${name}</h1>
+     <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github));
