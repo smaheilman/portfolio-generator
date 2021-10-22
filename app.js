499e8e11a97f179ae04efeb97a098d0d26a05362
Shmee <smaheilman@gmail.com>
2021-10-21 21:05:56 -0400

    Inquirer

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
-
-const generatePage = require('./src/page-template.js');
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-
-    console.log("Portfolio complete! Check out index.html to see the output!")
-});
+//const fs = require('fs');
+//
+//const generatePage = require('./src/page-template.js');
+//
+//fs.writeFile('index.html', generatePage(name, github), err => {
+//    if(err) throw err;
+//
+//    console.log("Portfolio complete! Check out index.html to see the output!")
+//});
