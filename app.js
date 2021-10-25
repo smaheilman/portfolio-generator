d75e1e1b0a86bf1de60a4c89dda8b5c1f9b97073
Shmee <smaheilman@gmail.com>
2021-10-25 14:26:09 -0400

    updated html

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -40,29 +42,22 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
-            when: ({confirmAbout}) => {
-                if(confirmAbout) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
+            when: ({confirmAbout}) => { confirmAbout
             }
         }
     ]);
 };
 
 const promptProject = portfolioData => {
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
-    }
-
     console.log(`
     ==================
     Add a New Project
     ==================
     `);
 
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
     return inquirer.prompt([
         {
             type: 'input',
@@ -139,14 +134,10 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
-//const fs = require('fs');
-//
-//const generatePage = require('./src/page-template.js');
-//
-//fs.writeFile('index.html', generatePage(name, github), err => {
-//    if(err) throw err;
-//
-//    console.log("Portfolio complete! Check out index.html to see the output!")
-//});
